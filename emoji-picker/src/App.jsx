df005dee66d09fcdff7be6bcb02e5c62a7dd5b38
Prem Choudhary <pk5480524@gmail.com>
2024-10-01 15:34:38 +0530

    last change in emoji-picker in emoji-picker branch

--- emoji-picker/src/App.jsx
+++ emoji-picker/src/App.jsx
@@ -1,11 +1,54 @@
 
+import  { useState } from 'react';
+// import './App.css';
+import EmojiPicker from 'emoji-picker-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
 
+  const [choosenEmoji, setChoosenEmoji] = useState('');
+
+  const copyEmojiFunction = (text) => {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+  };
+
+  const emojiPickerFunction = (emojiObject) => {
+      const emoji = emojiObject.emoji;
+      setChoosenEmoji(emoji);
+      copyEmojiFunction(emoji);
+      toast.success('Copied to Clipboard!', {
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: false,
+          progress: undefined,
+      });
+  };
+
   return (
     <>
-      
+       <div className="emoji-app">
+            <h1>send to your favourite </h1>
+            {choosenEmoji && (
+                <div className="selected-emoji">
+                    <p>Selected Emoji:</p>
+                    <span>{choosenEmoji}</span>
+                </div>
+            )}
+            <div className="emoji-picker">
+                <EmojiPicker onEmojiClick={emojiPickerFunction} />
+            </div>
+            <ToastContainer />
+        </div>
     </>
   )
 }
