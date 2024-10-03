f3de31d33fbc7bccf53c333a82b534ac4d0144be
Prem Choudhary <pk5480524@gmail.com>
2024-10-03 14:21:42 +0530

    home page

--- html-pages/src/App.jsx
+++ html-pages/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HtmlPages from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +10,10 @@ function App() {
   return (
     <>
       <div>
+      <HtmlPages/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a> 
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
